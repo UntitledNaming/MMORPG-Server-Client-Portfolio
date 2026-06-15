e424ba9ea17325661b64d58d4ff33e3a31f4178a
UntitledNaming <117499155+UntitledNaming@users.noreply.github.com>
2026-06-16 02:23:40 +0900

    00

--- Server/MMORPG_GameServer/FieldDropItemPool.cpp
+++ Server/MMORPG_GameServer/FieldDropItemPool.cpp
@@ -21,7 +21,7 @@ void FieldDropItemPool::Destroy()
 
 FieldDropItem* FieldDropItemPool::CreateItem(const Location& dropLocation)
 {
-    ITEM_ID retID = ItemIDConst::NORMAL_HELMET;//= GetItemID();
+    ITEM_ID retID = GetItemID();
     if (retID == 0)
         return nullptr;
 
